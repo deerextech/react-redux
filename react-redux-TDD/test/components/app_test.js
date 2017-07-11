308096896b54aec88ef2f1962d921012e30e987b
Danielle Stiring <deerextech@gmail.com>
2017-07-11 18:16:30 -0400

    1. Added index.html file 2. Configured a test helper file to use jsdom, render components, and simulate chai-query events. --- Probably going to need to update this once reducers are added. --- 3. Set up initial app_test.js just to check that everything is set up properly -- checks that a dummy component was rendered & checks that it contains 'some text' 4. Wrote out basic component app.js & render it in index.js 5. Initial set up tests pass now.

---
+++ react-redux-TDD/test/components/app_test.js
@@ -0,0 +1,23 @@
+import { renderComponent, expect } from '../test_helper';
+
+import App from '../../src/components/app';
+
+describe('App', ()=>{
+  //need to render component.
+  //check if it exists to start off.
+  let component; // set to undefined so it is initialized ahead of time
+  beforeEach(()=>{
+    // cant declare variable here because it would only be available in this function's scope.
+    component = renderComponent(App);
+
+  });
+
+  it('exists', ()=>{
+    expect(component).to.exist;
+    console.log('component*** ', component);
+  });
+  it('has the correct text', ()=>{
+    expect(component).to.contain('some text');
+  })
+
+})
---
+++ react-redux-TDD/test/test_helper.js
@@ -0,0 +1,66 @@
+import _$ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import jsdom from 'jsdom';
+import chai, { expect } from 'chai';
+import chaiJquery from 'chai-jquery';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers'; // future reducers file.
+
+//Set up testing environment to run like a browser in the command line
+//make fake browser. (global.document === window.document in the browser);
+// assign it to document, and put in html skeleton.
+//initializes and sets up fake browser for use @ commandline.
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+//return window obj associated w/ document. ^^^^
+global.window = global.document.defaultView;
+
+//reference Navigator obj.
+global.navigator = global.window.navigator;
+
+// _$ instead of just $ because $ reaches to browser and find DOM and will fail.
+// tell jquery make use of window provided not the browser.
+const $ = _$(global.window); //just be responsible for global.window set up by jsDom.
+
+/* End of fake DOM. */
+
+//set up chai jquery.
+chaiJquery(chai, chai.util, $);
+
+// build 'renderComponent' helper that should render given react class
+function renderComponent(ComponentClass, props = {}, state = {}) {
+  //ComponentClass refers to component class build (ex. CommentBox extends Component)
+  //make instance of said class (below)
+  //renderIntoDocument requires DOM, need react-dom library.
+  const componentInstance =  TestUtils.renderIntoDocument(
+  // probably going to need to wrap this in Provider once reducers come into play.... 
+    <ComponentClass {...props} />
+
+  );
+  //find & return DOM node.
+  //ReactDOM.findDOMNode gives reference to html
+  //wrapped in jQuery to connect it to helpers from jQuery Chai.
+  return $(ReactDOM.findDOMNode(componentInstance));
+}
+
+// helper for simulating events
+$.fn.simulate = function(eventName, value) {
+  //works with jquery el & takes event name & value from simulate events.
+  //$.fn. every jquery instance will have access to simulate function.
+  if (value) {
+    //this gives reference to element that is selected (and use it to get its value and val to set it.)
+    this.val(value);
+  }
+  //takes event name & element it is called on.
+  //reference object property so it can take whatever event happens.
+  TestUtils.Simulate[eventName](this);
+};
+
+//export to use in test suites.
+export {renderComponent, expect};
+
+
+//mocha: Runs the tests: load tests, run them one by one, clean up after each
+//chai: helpers for asserting certain properties about test subjects.
