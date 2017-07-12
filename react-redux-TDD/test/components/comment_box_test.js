519e8a06733bbe8d3db1c496c421eab862152a90
Danielle Stiring <deerextech@gmail.com>
2017-07-11 21:46:25 -0400

    tests for simulating change event (when text is entered) and check that text (new value) has proper value. Simulates test for a form submit.

--- react-redux-TDD/test/components/comment_box_test.js
+++ react-redux-TDD/test/components/comment_box_test.js
@@ -14,9 +14,29 @@ describe('CommentBox', ()=>{
 
   it('should have a button', ()=>{
     expect(component.find('button')).to.exist;
-  })
+  });
   it('should have class called comment_box ', ()=>{
     expect(component).to.have.class('comment_box');
-  })
+  });
+
+  describe('entering some text', ()=>{
+    //top beforeEach, nested For each, run before each it block.
+      beforeEach(()=>{
+        component.find('textarea').simulate('change', 'new comment');
+
+        /// simulating a change event.
+      });
+
+      it('shows that text in the textarea', ()=>{
+        expect(component.find('textarea')).to.have.value('new comment');
+      });
+
+      it('when submitted, clears the input', ()=>{
+        // component.find('button').simulate('onClick', '');
+        component.simulate('submit');
+        expect(component.find('textarea')).to.have.value('');
+      });
+  });
+
 
 });
